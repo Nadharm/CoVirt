915f366666429c117836b7a1eb4f3a0d85095561
Nadharm <52044930+Nadharm@users.noreply.github.com>
2022-04-30 00:09:50 -0500

    (non-SVM) Instruction Intercepts

--- src/vmcb.h
+++ src/vmcb.h
@@ -10,82 +10,117 @@ typedef struct salb {
 } __attribute__((packed)) salb_t;  // I need a better name for this...
 
 typedef struct ctrl_reg {
-	int cr0 	: 1;
-	int cr1 	: 1;
-	int cr2 	: 1;
-	int cr3 	: 1;
-	int cr4 	: 1;
-	int cr5 	: 1;
-	int cr6 	: 1;
-	int cr7 	: 1;
-	int cr8		: 1;
-	int cr9 	: 1;
-	int cr10 	: 1;
-	int cr11 	: 1;
-	int cr12 	: 1;
-	int cr13 	: 1;
-	int cr14 	: 1;
-	int cr15 	: 1;
+	int CR0 	: 1;
+	int CR1 	: 1;
+	int CR2 	: 1;
+	int CR3 	: 1;
+	int CR4 	: 1;
+	int CR5 	: 1;
+	int CR6 	: 1;
+	int CR7 	: 1;
+	int CR8		: 1;
+	int CR9 	: 1;
+	int CR10 	: 1;
+	int CR11 	: 1;
+	int CR12 	: 1;
+	int CR13 	: 1;
+	int CR14 	: 1;
+	int CR15 	: 1;
 } __attribute__((packed)) ctrl_reg_t;
 
 typedef struct debug_reg {
-	int dr0		: 1;
-	int dr1		: 1;
-	int dr2		: 1;
-	int dr3		: 1;
-	int dr4		: 1;
-	int dr5		: 1;
-	int dr6		: 1;
-	int dr7		: 1;
-	int dr8		: 1;
-	int dr9		: 1;
-	int dr10	: 1;
-	int dr11	: 1;
-	int dr12	: 1;
-	int dr13	: 1;
-	int dr14	: 1;
-	int dr15	: 1;
+	int DR0		: 1;
+	int DR1		: 1;
+	int DR2		: 1;
+	int DR3		: 1;
+	int DR4		: 1;
+	int DR5		: 1;
+	int DR6		: 1;
+	int DR7		: 1;
+	int DR8		: 1;
+	int DR9		: 1;
+	int DR10	: 1;
+	int DR11	: 1;
+	int DR12	: 1;
+	int DR13	: 1;
+	int DR14	: 1;
+	int DR15	: 1;
 } __attribute__((packed)) debug_reg_t; 
 
 typedef struct excp_vecs {
 	// AMD Volume 2 Section 8.2
 	// interrupt vector | name | cause
-	// "ex<NUM>" = reserved/undefined vector
+	// "EX<NUM>" = reserved/undefined vector
 
-	int de		: 1; // (0) Divide-by-Zero
-	int	db 		: 1; // (1) Debug ; Instruction + data accesses
-	int nmi		: 1; // (2) External Non-maskable interrupt
-	int bp		: 1; // (3) Breakpoint ; INT3 instruction
-	int of		: 1; // (4) Overflow ; INTO instruction
-	int br		: 1; // (5) Bound-range ; BOUND instruction
-	int ud		: 1; // (6) Invalid opcode/instruction
-	int nm		: 1; // (7) Device not avail ; x87 instructions
-	int df 		: 1; // (8) Double fault ; exception during handling of another fault
-	int ex9		: 1; // 
-	int ts		: 1; // (10) Invalid TSS ; task-state segment access + task switch
-	int np 		: 1; // (11) Segment-Not-Present 
-	int ss 		: 1; // (12) Stack ; ss register loads and stack references
-	int gp		: 1; // (13) General-Protection ; mem accesses + protection checks
-	int pf		: 1; // (14) Page Fault ; mem accessses when paging enabled
-	int ex15	: 1;
-	int mf		: 1; // (16) x87 floating point exception pending ;
-	int ac		: 1; // (17) alignment check ; misaligned mem addr
-	int mc		: 1; // (18) machine check; model specific
-	int xf		: 1; // (19) SIMD Floating-point ; SSE floating-point instructions
-	int ex20	: 1;
-	int cp 		: 1; // (21) Control-Protection Exception ; RET/IRET or other control transfer
-	int ex22	: 1;
-	int ex23	: 1;
-	int ex24	: 1;
-	int ex25	: 1;
-	int ex26	: 1;
-	int ex27	: 1;
-	int hv		: 1; // (28) Hypervisor Injection ; Event injection 
-	int vc		: 1; // (29) VMM Communication Exception ; Virtualization Event
-	int sx		: 1; // (30) Security Exception ; Security-sensitive event in host 
-	int ex31	: 1;
-	
+	int DE		: 1; // (0) Divide-by-Zero
+	int	DB 		: 1; // (1) Debug ; Instruction + data accesses
+	int NMI		: 1; // (2) External Non-maskable interrupt
+	int BP		: 1; // (3) Breakpoint ; INT3 instruction
+	int OF		: 1; // (4) Overflow ; INTO instruction
+	int BR		: 1; // (5) Bound-range ; BOUND instruction
+	int UD		: 1; // (6) Invalid opcode/instruction
+	int NM		: 1; // (7) Device not avail ; x87 instructions
+	int DF 		: 1; // (8) Double fault ; exception during handling of another fault
+	int EX9		: 1; // 
+	int TS		: 1; // (10) Invalid TSS ; task-state segment access + task switch
+	int NP 		: 1; // (11) Segment-Not-Present 
+	int SS 		: 1; // (12) Stack ; ss register loads and stack references
+	int GP		: 1; // (13) General-Protection ; mem accesses + protection checks
+	int PF		: 1; // (14) Page Fault ; mem accessses when paging enabled
+	int EX15	: 1;
+	int MF		: 1; // (16) x87 floating point exception pending ;
+	int AC		: 1; // (17) alignment check ; misaligned mem addr
+	int MC		: 1; // (18) machine check; model specific
+	int XF		: 1; // (19) SIMD Floating-point ; SSE floating-point instructions
+	int EX20	: 1;
+	int CP 		: 1; // (21) Control-Protection Exception ; RET/IRET or other control transfer
+	int EX22	: 1;
+	int EX23	: 1;
+	int EX24	: 1;
+	int EX25	: 1;
+	int EX26	: 1;
+	int EX27	: 1;
+	int HV		: 1; // (28) Hypervisor Injection ; Event injection 
+	int VC		: 1; // (29) VMM Communication Exception ; Virtualization Event
+	int SX		: 1; // (30) Security Exception ; Security-sensitive event in host 
+	int EX31	: 1;
+
 } __attribute__((packed)) excp_vecs_t;
+
+typedef struct instr_intercepts {
+	int INTR		: 1;  // physical maskable interrupt
+	int NMI			: 1;
+	int SMI			: 1;
+	int INIT		: 1;
+	int VINTR		: 1;  // virtual maskable interrupt
+	int CR0			: 1;  // intercept CR0 writes on bits other than CR0.TS or CR0.MP
+	int IDTR_RD		: 1;
+	int GDTR_RD 	: 1;
+	int LDTR_RD 	: 1;
+	int TR_RD 		: 1;
+	int IDTR_WR		: 1;
+	int GDTR_WR		: 1;
+	int LDTR_WR		: 1;
+	int TR_WR		: 1;
+	int RDTSC		: 1;
+	int RDPMC		: 1;
+	int PUSHF		: 1;
+	int POPF		: 1; 
+	int CPUID		: 1;
+	int RSM			: 1;
+	int IRET		: 1; 
+	int INTn		: 1;
+	int INVD		: 1;
+	int PAUSE		: 1;
+	int HLT			: 1;
+	int INVLPG		: 1;
+	int INVLPGA		: 1;
+	int IOIO_PROT	: 1;  // IN/OUT access of selected ports
+	int MSR_PROT	: 1;  // intercept RDMSR/WRMSR access to selected MSRs
+	int TASK_SWITCH	: 1;  // intercept task switches
+	int FERR_FREEZE	: 1;  // intercept processor "frezing" during legacy FERR handling
+	int SHUTDOWN	: 1;  // intercept shutdown events
+} __attribute__((packed)) instr_intercepts_t;
 
 // We'll eventually want to have structs defined for each type.
 typedef union efer {
@@ -94,12 +129,19 @@ typedef union efer {
 } efer_t; 
 
 typedef struct control_area {
+	// 0x000
 	ctrl_reg_t cr_reads;	
 	ctrl_reg_t cr_writes;
 
+	// 0x004
 	debug_reg_t dr_reads;
 	debug_reg_t dr_writes;
 
+	// 0x008
+	excp_vecs_t exception_vectors;
+
+	// 0x00C
+	instr_intercepts_t instr_intercepts;
 
 } control_area_t;
 
