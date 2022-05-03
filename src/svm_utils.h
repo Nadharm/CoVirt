912c251c773545d4c24b1cbda9d45b73f5b8c27f
Nadharm <52044930+Nadharm@users.noreply.github.com>
2022-05-02 23:49:07 -0500

    Random TODO page for better organization :D i'm so sleepy.

--- src/svm_utils.h
+++ src/svm_utils.h
@@ -1,6 +1,5 @@
 #include <linux/kernel.h>
-#include <linux/slab.h>
-#include <asm/io.h>
+
 
 int svm_check(void);  // Check if SVM is available or a possibility
 int enable_svm(void);  // Turn on SVM (flip EFER.SVME to 1)
