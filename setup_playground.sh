abe66c503f3f4a89a63fbedd1eda308ae8c26aef
Nadharm <52044930+Nadharm@users.noreply.github.com>
2022-05-06 15:24:33 -0500

    Setup script for testing env. BusyBox 1.33.2 & Linux 5.13.0.

---
+++ setup_playground.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Taken from Peter's guide. Ty <3
+
+apt install flex -y
+apt install bison -y
+
+mkdir tinylinux
+cd tinylinux
+export PLAY=`pwd`
+
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.13.tar.xz
+tar xf linux-5.13.tar.xz
+ln -s linux-5.13 linux
+
+wget https://busybox.net/downloads/busybox-1.33.2.tar.bz2
+tar xf busybox-1.33.2.tar.bz2
+ln -s busybox-1.33.2 busybox
+
+mkdir -pv obj/busybox
+
+cd busybox
+make O=../obj/busybox defconfig
+
+make O=../obj/busybox menuconfig
+
+# This will bring up a menu system.  Navigate to Settings -> Build static binary (no shared libs)  Turn this option on.   Then exit and be sure to say "yes" when it asks you to save your configuration.
+
+cd $PLAY/obj/busybox
+make
+make install
+
+cd $PLAY
+mkdir -pv initramfs/busybox
+
+cd initramfs/busybox
+mkdir -pv {bin,sbin,etc,proc,sys,usr/{bin,sbin}}
+
+cp -av $PLAY/obj/busybox/_install/* .
+
+wget http://pdinda.org/Stuff/init
+
+chmod +x init
+
+find . -print0 | cpio --null -ov --format=newc | gzip -9 > $PLAY/obj/initramfs-busybox.cpio.gz
+
+cd $PLAY/linux
+make O=../obj/linux-basic x86_64_defconfig
+
+cd $PLAY/linux
+make O=../obj/linux-basic menuconfig
+
+cd $PLAY/linux
+make O=../obj/linux-basic -j 4
+
+
+# Example Run:
+# cd $PLAY
+# qemu-system-x86_64 -m 2048 -smp 4 -kernel obj/linux-basic/arch/x86_64/boot/bzImage -initrd obj/initramfs-busybox.cpio.gz
+
